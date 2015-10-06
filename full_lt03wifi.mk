2a11f7e1ea5ec60d428d42a80a4b4a5e5fa7ecd8
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-06 16:07:53 +0200

    lt03wifi: Add AOSP makefiles
    Change-Id: I2ee4bc53e7d174517b642580a3e445823e805172

---
+++ full_lt03wifi.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2015 TeamEOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/lt03wifi/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := full_lt03wifi
+PRODUCT_DEVICE := lt03wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P600
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=lt03wifi
